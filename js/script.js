c16ff274d4f631514284023fd8dabc7ec02b8a33
Giadagenovesi <giada.g@outlook.it>
2023-03-09 16:01:33 +0100

    Completed milestone 1

--- js/script.js
+++ js/script.js
@@ -6,7 +6,6 @@
 // - va applicato uno sconto del 20% per i minorenni
 // - va applicato uno sconto del 40% per gli over 65.
 
-
 //INPUT E DICHIARAZIONE DEI DATI
 const kmPrice = 0.21;
 
@@ -15,20 +14,40 @@ const kmPrice = 0.21;
 const sumbitBtn = document.getElementById("submit-btn");
 sumbitBtn.addEventListener("click", function() {
     const result = document.getElementById("result");
-    // Ripulire il result
+    // Resettare il risultato
     result.className = "";
 
-    // Prendere il valore del input.
+    // Prendere il valore degli input.
     const kmNumberInput = document.getElementById("km-number");
     const kmNumber = parseInt (kmNumberInput.value);
 
     const userAgeInput = document.getElementById("user-age");
-    const userAge = userAgeInput.value;
+    const userAge = parseInt (userAgeInput.value);
 
     console.log(kmNumber, userAge);
 
+    //LOGICA
+    //Prezzo biglietto
+
+    const basePrice = kmPrice * kmNumber;
+    console.log(basePrice);
+
+    let ticketPrice = basePrice;
+    if (userAge < 18) {
+        ticketPrice = basePrice - (basePrice * 20 / 100);
+    } else if (userAge >= 65) {
+        ticketPrice = basePrice - (basePrice * 40 / 100)
+    }
+    console.log(ticketPrice);
+
+    const message = "Il prezzo del tuo biglietto è di €" + ticketPrice.toFixed (2)
+    console.log (message)
 
     // Ripulisco i campi di input
     kmNumberInput.value = "";
     userAgeInput.value = "";
 });
+    
+
+
+   
